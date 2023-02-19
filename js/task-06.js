c11497cac0d1a257b56e0ec526cfcfa1482fc393
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-02-19 13:26:01 +0200

    fixed validation on 6 tasks

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,17 @@
 const inputRef = document.querySelector('#validation-input');
 const dataLength = document.querySelector('[data-length]');
 const keybordClick = (event) => {
-  if (dataLength.dataset.length <= inputRef.value.length){
-
+  if (Number.parseInt(dataLength.dataset.length) === inputRef.value.length){
+     
+      
 
    event.currentTarget.classList.remove("invalid");
       event.currentTarget.classList.add("valid");
 } else {
    event.currentTarget.classList.remove("valid");
     event.currentTarget.classList.add("invalid");
-      
+    
 };  
 };
 inputRef.addEventListener("blur", keybordClick);
+
